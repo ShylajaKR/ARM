988ce5e3a96ea9be244ba26b0fc4e802aa63f06a
ShylajaKR <44345064+ShylajaKR@users.noreply.github.com>
2018-11-20 23:21:57 +0530

    Add files via upload

---
+++ Assignments/Assignment_5/LogicGatesNN.s
@@ -0,0 +1,126 @@
+     AREA     appcode, CODE, READONLY
+	 IMPORT printMsg
+     EXPORT __main
+	 ENTRY 
+	 
+Sigma PROC	
+	;e^x is being computed for x=7 and n=12.. Final answer is in "S24" register.
+	;Hex value = 0x448560C7 Floating point value = 1067.0242919921875 and it matches the theoretical value.
+
+	VMOV.F32 S21, #25	;S21 contains the number of terms (n) for which the e^x expansion is being done
+	VMOV.F32 S22, #1	;S22 is the count which is incremented until S21 value is reached. 
+	VMOV.F32 S23, #1	;S23 holds the temporary result (x^n)/n factorial
+	VMOV.F32 S24, #1	;S24 holds the final result of e^x of n terms
+	VMOV.F32 S26, #1	;S26 has 1 in order to increment the value of S22 
+Loop 
+	 VCMP.F32 S21, S22; S21 has no.of terms.S22 is compared with S21 to check whether n computations has been done
+	 VMRS.F32 APSR_nzcv,FPSCR; FPU affects FPSCR which is status register for floating point unit. In order to check  
+	 ;whether S22 has reached S21 we have to make use of flags. Therefore FPSCR has been copied into APSR status flag
+	 ;register
+	 BLT sigmoid			;If S22 has become greater than S21 then branch to stop and stop the execution
+	 VDIV.F32 S25, S20, S22;  S20 has x and S22 has the count. So x/1,x/2 and so on till x/n is being done and stored in S25
+	 VMUL.F32 S23, S23, S25; Initially S23 has 1 so 1*(x/1) is multiplied and stored back to S23. Next iteration (x/1)*(x/2) 
+	 ;is computed nothing but (x^2)/2!.. And so on till (x^n)/n!
+	 VADD.F32 S24, S24, S23; S24 initially contains 1. So now 1+(x/1!) is being added and finally 1+(x/1!)+(x^2/2!)+..(x^n/n!)
+	 VADD.F32 S22, S22, S26; Here S22 is incremented by 1 S22=S22+1
+	 B Loop; Unconditional branch back to loop until n terms are not computed.
+	 
+	 ;Computing sigmoid function
+sigmoid
+	 VADD.F32 S27,S24,S26
+	 VDIV.F32 S28,S24,S27 ;S28 will hold the sigmoid value.
+	 
+	 VMOV.F32 R0,S28
+	 BX LR
+	 ENDP
+		 
+__main  FUNCTION
+	 ; x1 x2 x3 are the logical inputs and x4=1 bias
+	 VLDR.F32 S0,=1
+	 VLDR.F32 S1,=0
+	 VLDR.F32 S2,=0
+	 VMOV.F32 S3, #1
+	 
+	 ;This copies the address of table to R1
+	 ADR.W R1,Branchtable_byte
+	 MOV R2,#2
+	 TBB[R1,R2] ;Point to row 2 in the table pointed by R1
+;Equivalent to case statements 
+;w1 w2 w3 and w4..different for all the 7 gates
+ANDgate	
+	 VLDR.F32 S4, =-0.1
+	 VLDR.F32 S5, =0.2
+	 VLDR.F32 S6, =0.2
+	 VLDR.F32 S7, =-0.2
+	 B cacl_Z
+
+ORgate
+	 VLDR.F32 S4, =-0.1
+	 VLDR.F32 S5, =0.7
+	 VLDR.F32 S6, =0.7
+	 VLDR.F32 S7, =-0.1
+	 B cacl_Z
+	 
+NOTgate
+	 VLDR.F32 S4, =-0.7
+	 VLDR.F32 S5, =0
+	 VLDR.F32 S6, =0
+	 VLDR.F32 S7, =0.1
+	 B cacl_Z
+	 
+NANDgate
+	 VLDR.F32 S4, =0.6
+	 VLDR.F32 S5, =-0.8
+	 VLDR.F32 S6, =-0.8
+	 VLDR.F32 S7, =0.3
+	 B cacl_Z
+	 
+NORgate
+	 VLDR.F32 S4, =0.5
+	 VLDR.F32 S5, =-0.7
+	 VLDR.F32 S6, =-0.7
+	 VLDR.F32 S7, =0.1
+	 B cacl_Z
+	 
+XORgate
+	 VLDR.F32 S4, =5
+	 VLDR.F32 S5, =-20
+	 VLDR.F32 S6, =-10
+	 VLDR.F32 S7, =-1
+	 B cacl_Z
+	 
+XNORgate 
+	 VLDR.F32 S4, =-5
+	 VLDR.F32 S5, =20
+	 VLDR.F32 S6, =10
+	 VLDR.F32 S7, =1
+	 B cacl_Z
+	 
+;Table which holds the address of all the case statements
+Branchtable_byte		  
+    DCB   0						;0 points to AND logic function  
+    DCB   ((ORgate-ANDgate)/2)	;This is the offset for OR function and similarly for all other gates
+	DCB   ((NOTgate-ANDgate)/2)	
+	DCB   ((NANDgate-ANDgate)/2)
+	DCB   ((NORgate-ANDgate)/2)
+	DCB   ((XORgate-ANDgate)/2)
+	DCB   ((XNORgate-ANDgate)/2)    
+	 
+;Weights are used and Z is calculated in this section of the code
+cacl_Z	 VMUL.F32 S8, S0,S4  ;x1*w1
+		 VMUL.F32 S9, S1,S5  ;x2*w2
+		 VMUL.F32 S10, S2,S6 ;x3*w3
+		 VMUL.F32 S11, S3,S7 ;x4*w4
+	 
+		 VADD.F32 S12, S8, S9   ;(x1*w1)+(x2*w2)
+		 VADD.F32 S13, S10, S11 ;(x3*w3)+(x4*w4)
+	 
+		 VADD.F32 S20, S12, S13 ; Z is in S20 register which is sent as input to sigmoid subroutine
+	 
+		 ; call the sigmoid subroutine from the main function
+		 BL Sigma
+		 BL printMsg	 ; Refer to ARM Procedure calling standards.
+	 
+stop B stop ; stop program
+	 ENDFUNC
+	 END
